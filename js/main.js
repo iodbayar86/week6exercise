dfabd116dec37b85f4e72739cc4845e9fc6ba99f
Odbayar Odnamar <iodbayar86@gmail.com>
2018-05-05 12:33:11 -0700

    first commit

---
+++ js/main.js
@@ -0,0 +1,43 @@
+var tetOne = "What kind of variable am I?";
+
+var testTwo = 256;
+
+var testThree = false;
+
+var testFour = 'true';
+
+var testFive ='128';
+
+var testSix ="";
+
+
+
+var testOne = "hello" + "there" ;
+
+var testTwo = '100' + 28  ;
+
+var testThree = true + 3  ;
+
+var testFour = 'false' + 5 ;
+
+var testFive = '128' - 3 ;
+
+var testSix = "" * 9  ;
+
+var testSeven = 'zero' - 1 ;
+
+var testEight=   ;
+
+var testNine =   ;
+
+var testTen =   ;
+
+var testEleven =   ;
+
+var testTwelve =   ;
+
+var testThirteen =   ;
+
+var testFourteen =   ;
+
+var testFifteen =   ;
